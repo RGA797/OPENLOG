e0043e26ad170b5b2058c01d2ff85d18dc1fe8f9
Rachid Gaim Ahwaz <s183925@dtu.dk>
2021-10-27 14:28:07 +0200

    updated UI

--- app/src/main/java/com/example/openlog/opret_profil.kt
+++ app/src/main/java/com/example/openlog/opret_profil.kt
@@ -13,10 +13,10 @@ private const val ARG_PARAM2 = "param2"
 
 /**
  * A simple [Fragment] subclass.
- * Use the [Opret_bruger.newInstance] factory method to
+ * Use the [opret_profil.newInstance] factory method to
  * create an instance of this fragment.
  */
-class Opret_bruger : Fragment() {
+class opret_profil : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
@@ -34,7 +34,7 @@ class Opret_bruger : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_opret_bruger, container, false)
+        return inflater.inflate(R.layout.fragment_opret_profil, container, false)
     }
 
     companion object {
@@ -44,12 +44,12 @@ class Opret_bruger : Fragment() {
          *
          * @param param1 Parameter 1.
          * @param param2 Parameter 2.
-         * @return A new instance of fragment Opret_bruger.
+         * @return A new instance of fragment opret_profil.
          */
         // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
-            Opret_bruger().apply {
+            opret_profil().apply {
                 arguments = Bundle().apply {
                     putString(ARG_PARAM1, param1)
                     putString(ARG_PARAM2, param2)
