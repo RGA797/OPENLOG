502c5613754185ca6d0a2ffcc22a3715bb3cb67b
GulsenT <s194612@student.dtu.dk>
2021-11-15 10:19:06 +0100

    Implemented graphviews with notes for how to use it

--- app/src/main/java/com/example/openlog/MainActivity.kt
+++ app/src/main/java/com/example/openlog/MainActivity.kt
@@ -17,12 +17,17 @@ class MainActivity : AppCompatActivity() {
 
         //arrayOf(DataPoint...) kan benyttes som viewmodel, mens selve grafen vises i view.
         //Data til DataPoint hentes fra en DB connector som derfra henter data fra en database
+        //Grafen indsættes vha. datapoints hvor x-værdien altid forøges,
+        // dvs. DataPoint(5,0, 1.0), DataPoint(4.0, 1.0) giver fejl og programmet crasher
+        //Graphviews der ikke vises i et bestemt view(eks. fragment_status_bar_kulhydrater)
+        //vil resultere i et NullPointerException
+
         //val graphInsulin = findViewById(R.id.graph_insulin) as GraphView
-        //val seriesInsulin = LineGraphSeries(arrayOf(DataPoint(0.0, 1.0), DataPoint(1.0,5.0), DataPoint(2.0, 3.0)))
+        //val seriesInsulin = LineGraphSeries(arrayOf(DataPoint(1.0, 1.0), DataPoint(1.0,5.0), DataPoint(2.0, 3.0)))
         //graphInsulin.addSeries(seriesInsulin)
 
         val graphKulhydrater = findViewById(R.id.graph_kulhydrater) as GraphView
-        val seriesKulhydrater = LineGraphSeries(arrayOf(DataPoint(4.0, 2.0), DataPoint(7.0, 4.0), DataPoint(3.0, 5.0)))
+        val seriesKulhydrater = LineGraphSeries(arrayOf(DataPoint(4.0, 2.0), DataPoint(7.0, 4.0), DataPoint(8.0, 5.0)))
         graphKulhydrater.addSeries(seriesKulhydrater)
 
         //val graphBlodsukker = findViewById(R.id.graph_blodsukker) as GraphView
