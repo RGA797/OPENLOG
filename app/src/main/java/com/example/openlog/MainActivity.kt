05e6c68820c3fbbc9d93f1658099a0c75bdf6675
Rachid131 <55383115+Rachid131@users.noreply.github.com>
2021-11-16 13:29:45 +0100

    cleaned up stock code. commented out faulty code

--- app/src/main/java/com/example/openlog/MainActivity.kt
+++ app/src/main/java/com/example/openlog/MainActivity.kt
@@ -13,7 +13,7 @@ class MainActivity : AppCompatActivity() {
         /* Test any desired layout by replacing the activity_main (xml)
            with another xml-file (ex. fragment_status_bar__insulin etc.)
         * */
-        setContentView(R.layout.fragment_status_bar__kulhydrater)
+        setContentView(R.layout.activity_main)
 
         //arrayOf(DataPoint...) kan benyttes som viewmodel, mens selve grafen vises i view.
         //Data til DataPoint hentes fra en DB connector som derfra henter data fra en database
@@ -21,9 +21,9 @@ class MainActivity : AppCompatActivity() {
         //val seriesInsulin = LineGraphSeries(arrayOf(DataPoint(0.0, 1.0), DataPoint(1.0,5.0), DataPoint(2.0, 3.0)))
         //graphInsulin.addSeries(seriesInsulin)
 
-        val graphKulhydrater = findViewById(R.id.graph_kulhydrater) as GraphView
-        val seriesKulhydrater = LineGraphSeries(arrayOf(DataPoint(4.0, 2.0), DataPoint(7.0, 4.0), DataPoint(3.0, 5.0)))
-        graphKulhydrater.addSeries(seriesKulhydrater)
+        //val graphKulhydrater = findViewById(R.id.graph_kulhydrater) as GraphView
+        //val seriesKulhydrater = LineGraphSeries(arrayOf(DataPoint(4.0, 2.0), DataPoint(7.0, 4.0), DataPoint(3.0, 5.0)))
+        //graphKulhydrater.addSeries(seriesKulhydrater)
 
         //val graphBlodsukker = findViewById(R.id.graph_blodsukker) as GraphView
         //val seriesBlodsukker = LineGraphSeries(arrayOf(DataPoint(1.0, 2.0), DataPoint(4.0, 5.0), DataPoint(6.0, 3.0)))
