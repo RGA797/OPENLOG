8e704c0da06f2da901f7b14ed3923f079f23462c
Rachid Gaim Ahwaz <s183925@dtu.dk>
2021-11-21 16:34:09 +0100

    work on voice implementation(not working)

--- app/src/main/java/com/example/openlog/Forside.kt
+++ app/src/main/java/com/example/openlog/Forside.kt
@@ -1,6 +1,7 @@
 package com.example.openlog
 
 import android.app.Activity
+import android.content.ActivityNotFoundException
 import android.content.Intent
 import android.os.Bundle
 import android.speech.RecognizerIntent
@@ -12,6 +13,8 @@ import android.widget.Button
 import android.widget.ImageButton
 import android.widget.TextView
 import android.widget.Toast
+import androidx.activity.result.ActivityResult
+import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.Navigation
 import androidx.core.app.ActivityCompat.startActivityForResult
 import androidx.databinding.DataBindingUtil
@@ -35,12 +38,10 @@ class Forside : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?){
         binding.buttonWidget.setOnClickListener{ Navigation.findNavController(binding.root).navigate(R.id.navigateFromForsideToHistorik) }
-        binding.microphoneButton.setOnClickListener{speak()}
+        //binding.microphoneButton.setOnClickListener{speak()}
     }
 
 
-    private val REQUEST_CODE_SPEECH_INPUT = 100
-    //made with the following tutorials and questions:https://newbedev.com/using-intent-to-call-a-fragment-from-another-fragment , https://www.youtube.com/watch?v=Wv2hafcjEVY,          https://stackoverflow.com/questions/18329104/recognizerintent-change-default-language
     fun speak(){
         val mIntent = Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH)
         val language = "dk-DK"
@@ -49,25 +50,20 @@ class Forside : Fragment() {
         mIntent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_PREFERENCE, language)
         mIntent.putExtra(RecognizerIntent.EXTRA_ONLY_RETURN_LANGUAGE_PREFERENCE, language)
         mIntent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM)
-        mIntent.putExtra(RecognizerIntent.EXTRA_PROMPT, "Enter input")
-
-        startActivityForResult(mIntent, REQUEST_CODE_SPEECH_INPUT)
-
-    }
-
-
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?){
-        super.onActivityResult(requestCode, resultCode, data)
-        when(requestCode){
-            REQUEST_CODE_SPEECH_INPUT -> {
-                if (resultCode == Activity.RESULT_OK && null != data){
-                    val result = data.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS)
-               //     binding.speechText.text = result[0]
-                }
-            }
+        mIntent.putExtra(RecognizerIntent.EXTRA_PROMPT, "Enter something to log")
+        try {
+            activityResultLauncher.launch(mIntent)
+        }
+        catch (exp: ActivityNotFoundException){
+            Toast.makeText(applicationContext, "Device not supported", Toast.LENGTH_SHORT.show())
         }
     }
 
-
+    activityResultLauncher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()){val result: ActivityResult? ->
+        if (result!!.resultCode== AppCompatActivity.RESULT_OK && result!!.data!= null) {
+            val text =result!!.data!!.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS) as ArrayList<Editable>
+            binding.speechText.text = text[0]
+        }
+    }
 
 }
