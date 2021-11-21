3a65739c09527a1c212a2c173c803636357b78f8
Rachid131 <55383115+Rachid131@users.noreply.github.com>
2021-11-21 19:41:11 +0100

    work on speech recognition. some firebase (doesnt work)

--- app/src/main/java/com/example/openlog/Forside.kt
+++ app/src/main/java/com/example/openlog/Forside.kt
@@ -1,10 +1,10 @@
 package com.example.openlog
 
-import android.app.Activity
 import android.content.ActivityNotFoundException
 import android.content.Intent
 import android.os.Bundle
 import android.speech.RecognizerIntent
+import android.text.Editable
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -14,56 +14,61 @@ import android.widget.ImageButton
 import android.widget.TextView
 import android.widget.Toast
 import androidx.activity.result.ActivityResult
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.Navigation
-import androidx.core.app.ActivityCompat.startActivityForResult
-import androidx.databinding.DataBindingUtil
-import androidx.fragment.app.FragmentManager
-import androidx.fragment.app.activityViewModels
-import com.example.openlog.databinding.FragmentForsideBinding
-
 
 class Forside : Fragment() {
 
-    private lateinit var binding: FragmentForsideBinding
+    lateinit var activityResultLauncher: ActivityResultLauncher<Intent>
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
     //viewmodel is made here. using activitymodels for the instantiation, it can be shared among other fragments
+    // code largely gotten from https://www.youtube.com/watch?v=YWNkNrmqff8&ab_channel=Dr.VipinClasses
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_forside, container, false)
-        return binding.root
-    }
+        val view = inflater.inflate(R.layout.fragment_forside, container, false)
+        val buttonWidget = view.findViewById<ImageButton>(R.id.buttonWidget)
+        val microphoneButton =  view.findViewById<ImageButton>(R.id.microphoneButton)
+        val speechText = view.findViewById<TextView>(R.id.speechText)
 
-
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?){
-        binding.buttonWidget.setOnClickListener{ Navigation.findNavController(binding.root).navigate(R.id.navigateFromForsideToHistorik) }
-        //binding.microphoneButton.setOnClickListener{speak()}
-    }
-
-
-    fun speak(){
-        val mIntent = Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH)
-        val language = "dk-DK"
-        mIntent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, language)
-        mIntent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, language)
-        mIntent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_PREFERENCE, language)
-        mIntent.putExtra(RecognizerIntent.EXTRA_ONLY_RETURN_LANGUAGE_PREFERENCE, language)
-        mIntent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM)
-        mIntent.putExtra(RecognizerIntent.EXTRA_PROMPT, "Enter something to log")
-        try {
-            activityResultLauncher.launch(mIntent)
+        microphoneButton.setOnClickListener{
+            val mIntent = Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH)
+            val language = "dk-DK"
+            mIntent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, language)
+            mIntent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, language)
+            mIntent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_PREFERENCE, language)
+            mIntent.putExtra(RecognizerIntent.EXTRA_ONLY_RETURN_LANGUAGE_PREFERENCE, language)
+            mIntent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM)
+            mIntent.putExtra(RecognizerIntent.EXTRA_PROMPT, "Enter something to log")
+            try {
+                activityResultLauncher.launch(mIntent)
+            }
+            catch (exp: ActivityNotFoundException){
+                Toast.makeText(context, "Device not supported", Toast.LENGTH_SHORT).show()
+            }
         }
-        catch (exp: ActivityNotFoundException){
-            Toast.makeText(applicationContext, "Device not supported", Toast.LENGTH_SHORT.show())
+
+        activityResultLauncher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()){result: ActivityResult? ->
+            if (result!!.resultCode== AppCompatActivity.RESULT_OK && result!!.data!= null) {
+                val text =result.data!!.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS) as ArrayList<Editable>
+                speechText.text = text[0]
+                inputData(text[0])
+            }
         }
+        buttonWidget.setOnClickListener{ Navigation.findNavController(view).navigate(R.id.navigateFromForsideToHistorik) }
+        return view
     }
 
-    activityResultLauncher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()){val result: ActivityResult? ->
-        if (result!!.resultCode== AppCompatActivity.RESULT_OK && result!!.data!= null) {
-            val text =result!!.data!!.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS) as ArrayList<Editable>
-            binding.speechText.text = text[0]
+    fun inputData(data: Editable) {
+        if (data.contains("kulhydrat")){
+            val kulhydratInput = KulhydratInput(data)
+            database.child("kulhydraterInput").child("kulhydrat").setValue(kulhydratInput)
         }
     }
-
 }
---
+++ app/src/main/java/com/example/openlog/KulhydratInput.kt
@@ -0,0 +1,7 @@
+package com.example.openlog
+
+import android.text.Editable
+import java.util.*
+
+data class KulhydratInput (val data: Editable){
+}
