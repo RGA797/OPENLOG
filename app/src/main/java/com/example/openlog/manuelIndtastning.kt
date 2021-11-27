ad2930cc5493c5d49c74476daec859fcab971dea
Rachid Gaim Ahwaz <s183925@dtu.dk>
2021-11-27 17:20:26 +0100

    implemented manuelIndtastning with firebase database.

--- app/src/main/java/com/example/openlog/manuelIndtastning.kt
+++ app/src/main/java/com/example/openlog/manuelIndtastning.kt
@@ -1,23 +1,92 @@
 package com.example.openlog
 
 import android.os.Bundle
+import android.text.Editable
+import android.view.*
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.activityViewModels
+import androidx.navigation.Navigation
+import com.example.openlog.databinding.FragmentForsideBinding
+import com.example.openlog.databinding.FragmentManuelIndtastningBinding
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.database.FirebaseDatabase
+import java.text.SimpleDateFormat
+import java.util.*
 
 
 class manuelIndtastning : Fragment() {
-
+    private val dataViewModel: DataViewModel by activityViewModels()
+    private lateinit var binding: FragmentManuelIndtastningBinding
     override fun onCreate(savedInstanceState: Bundle?) {
+        setHasOptionsMenu(true)
         super.onCreate(savedInstanceState)
     }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_manuel_indtastning, container, false)
+    ): View {
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_manuel_indtastning, container, false)
+        return binding.root
     }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.logButton.setOnClickListener{onLog()}
+    }
+
+    fun onLog(){
+        val input = binding.textInput.text.toString()
+        val firebaseUser: FirebaseUser = dataViewModel.getCurrentFirebaseUser()!!
+        val database = FirebaseDatabase.getInstance("https://openlog-a2b24-default-rtdb.europe-west1.firebasedatabase.app/")
+        val sdf = SimpleDateFormat("dd/M/yyyy hh:mm:ss")
+        val time: String
+        val myRef = database.getReference("users").child(firebaseUser.uid).push()
+        if (input.contains("insulin")) {
+            time = sdf.format(Date())
+            val inputValue = input.filter { it.isDigit() }
+            if (inputValue != "") {
+                myRef.child(time).child("insulin").setValue(inputValue)
+            }
+
+            binding.textInput.setText("")
+            return
+
+        }
+        else if (input.contains("kulhydrat")) {
+            time = sdf.format(Date())
+            val inputValue = input.filter { it.isDigit() }
+            if (inputValue != "") {
+                myRef.child(time).child("kulhydrat").setValue(inputValue)
+            }
+            binding.textInput.setText("")
+            return
+        }
+        else if (input.contains("blodsukker")) {
+            time = sdf.format(Date())
+            val inputValue = input.filter { it.isDigit() }
+            if (inputValue != "") {
+                myRef.child(time).child("blodsukker").setValue(inputValue)
+            }
+            binding.textInput.setText("")
+            return
+        }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, menuInflater: MenuInflater){
+        menuInflater.inflate(R.menu.nav_menu,menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId){
+            R.id.historikDropdown -> onHistorikDropdown()
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+    fun onHistorikDropdown(){
+        Navigation.findNavController(binding.root).navigate(R.id.navigateFromManuelIndtastningToHistorik)
+    }
+
 }
