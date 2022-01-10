c7c3f4892415f686c1f30f646e8d85eddbeb1c71
Rachid Gaim Ahwaz <s183925@dtu.dk>
2022-01-10 16:26:54 +0100

    made databinding

--- app/src/main/java/com/example/openlog/view/Opret_profil.kt
+++ app/src/main/java/com/example/openlog/view/Opret_profil.kt
@@ -1,5 +1,6 @@
 package com.example.openlog.view
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -9,7 +10,9 @@ import android.widget.ArrayAdapter
 import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
+import androidx.activity.result.ActivityResultLauncher
 import androidx.core.text.isDigitsOnly
+import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.activityViewModels
 import com.google.android.gms.tasks.OnCompleteListener
 import com.google.firebase.auth.AuthResult
@@ -17,6 +20,8 @@ import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
 import androidx.navigation.Navigation
 import com.example.openlog.R
+import com.example.openlog.databinding.FragmentForsideBinding
+import com.example.openlog.databinding.FragmentOpretProfilBinding
 import com.example.openlog.viewModel.DataViewModel
 import com.google.firebase.database.FirebaseDatabase
 
@@ -24,6 +29,8 @@ class Opret_profil : Fragment() {
 
 
     private val dataViewModel: DataViewModel by activityViewModels()
+    lateinit var activityResultLauncher: ActivityResultLauncher<Intent>
+    private lateinit var binding: FragmentOpretProfilBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -32,43 +39,44 @@ class Opret_profil : Fragment() {
         val view = inflater.inflate(R.layout.fragment_opret_profil, container, false)
         // Inflate the layout for this fragment
 
-        val registerButton = view.findViewById<Button>(R.id.opretProfilButton)
-        val emailTextView = view.findViewById<EditText>(R.id.opretEmailText)
-        val adgangskodeTextView = view.findViewById<EditText>(R.id.opretAdgangskodeText)
-        val koenTextView = view.findViewById<EditText>(R.id.opretKønText)
-        val alderTextView = view.findViewById<EditText>(R.id.opretAlderText)
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_opret_profil, container, false)
+        return binding.root
+    }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+
+        binding.dataViewModel = dataViewModel
+        super.onViewCreated(view, savedInstanceState)
 
         //upon pressing the register button, a user is created. if succesful, optional values for age and gender is saved
-        registerButton.setOnClickListener {
-            if (emailTextView.text.isEmpty() || adgangskodeTextView.text.isEmpty()) {
-                Toast.makeText(context, "email eller adgangskode er tom", Toast.LENGTH_SHORT).show()
-            } else {
-                val email: String = emailTextView.text.toString()
-                val adgangskode: String = adgangskodeTextView.text.toString()
-                val koen: String = koenTextView.text.toString()
-                val alder: String = alderTextView.text.toString()
-
-                FirebaseAuth.getInstance().createUserWithEmailAndPassword(email, adgangskode)
-                    .addOnCompleteListener(requireActivity(), OnCompleteListener<AuthResult>() { task ->
-                        if (task.isSuccessful) {
-                            val firebaseUser: FirebaseUser = task.result!!.user!!
-                            dataViewModel.changeUser(firebaseUser, email)
-                            Toast.makeText(context, "Du er nu registreret", Toast.LENGTH_SHORT).show()
-                            Navigation.findNavController(view).navigate(R.id.navigateFromOpretToForside)
-                            if (alder.isNotBlank() && alder.isDigitsOnly() && koen.isNotEmpty() && alder.toInt() in 1..99){
-                                dataViewModel.saveUserData(firebaseUser, koen, alder)
-                            }
-                        } else {
-                            Toast.makeText(context, "E-mail eller adgangskode er ikke valit", Toast.LENGTH_SHORT).show()
-                        }
-                    })
-            }
+        binding.opretProfilButton.setOnClickListener {onOpretButton()
         }
 
         //val languages = resources.getStringArray(R.array.Køn)
         //val arrayAdapter = ArrayAdapter (requireContext(),R.layout.dropdown_item())
+    }
+    fun onOpretButton(){
+        if (binding.opretEmailText.text?.isEmpty() == true || binding.opretAdgangskodeText.text?.isEmpty() == true ) {
+            Toast.makeText(context, "email eller adgangskode er tom", Toast.LENGTH_SHORT).show()
+        } else {
+            val email: String = binding.opretEmailText.text.toString()
+            val adgangskode: String = binding.opretAdgangskodeText.text.toString()
+            val koen: String = binding.opretKNText.text.toString()
+            val alder: String = binding.opretAlderText.text.toString()
 
-
-        return view
+            FirebaseAuth.getInstance().createUserWithEmailAndPassword(email, adgangskode)
+                .addOnCompleteListener(requireActivity(), OnCompleteListener<AuthResult>() { task ->
+                    if (task.isSuccessful) {
+                        val firebaseUser: FirebaseUser = task.result!!.user!!
+                        dataViewModel.changeUser(firebaseUser, email)
+                        Toast.makeText(context, "Du er nu registreret", Toast.LENGTH_SHORT).show()
+                        Navigation.findNavController(binding.root).navigate(R.id.navigateFromOpretToForside)
+                        if (alder.isNotBlank() && alder.isDigitsOnly() && koen.isNotEmpty() && alder.toInt() in 1..99){
+                            dataViewModel.saveUserData(firebaseUser, koen, alder)
+                        }
+                    } else {
+                        Toast.makeText(context, "E-mail eller adgangskode er ikke valit", Toast.LENGTH_SHORT).show()
+                    }
+                })
+        }
     }
 }
