1b6f63d5bfd5aaa10292b7891e0cdc0fa620a5a3
Rachid Gaim Ahwaz <s183925@dtu.dk>
2022-01-11 13:00:03 +0100

    made date converter. implemented by Data and InputDTO classes

--- app/src/main/java/com/example/openlog/model/Data.kt
+++ app/src/main/java/com/example/openlog/model/Data.kt
@@ -101,39 +101,4 @@ class Data {
         }
         myRef.addValueEventListener(valueEventListener)
     }
-
-    //checks weather a date is in range of two other date
-    fun dateInRange(dateString: String, startDate: Date, endDate: Date): Boolean{
-        val date = convertStringToDate(dateString)
-        return (date.after(startDate) && date.before(endDate))
-    }
-
-    //converts a String to Date. this is used when inputDto dates need to be converted back into Dates for comparison, as they are stored as trings in the database.
-    fun convertStringToDate(time: String): Date {
-        val stringList = time.split(" ")
-        val day = stringList[2].toInt()
-        val month = when(stringList[1]){
-            "Jan" -> 0
-            "Feb" -> 1
-            "Mar" -> 2
-            "Apr" -> 3
-            "May" -> 4
-            "Jun" -> 5
-            "Jul" -> 6
-            "Aug" -> 7
-            "Sep" -> 8
-            "Oct" -> 9
-            "Now" -> 10
-            "Dec" -> 11
-            else -> {0}
-        }
-        val hourOfDay = stringList[3].split(":")[0].toInt()
-        val minuteOfDay = stringList[3].split(":")[1].toInt()
-        val seconOfDay = stringList[3].split(":")[2].toInt()
-
-        val year = stringList[5].toInt()
-        val date = Calendar.getInstance()
-        date.set(year, month, day, hourOfDay, minuteOfDay, seconOfDay)
-        return date.time
-    }
 }
