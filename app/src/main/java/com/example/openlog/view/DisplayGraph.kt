d0569dc181918ea9a6eca27f9c7ebc04afff83da
Rachid131 <55383115+Rachid131@users.noreply.github.com>
2022-01-19 01:04:19 +0100

    merge fix

--- app/src/main/java/com/example/openlog/view/DisplayGraph.kt
+++ app/src/main/java/com/example/openlog/view/DisplayGraph.kt
@@ -6,8 +6,11 @@ import android.content.ContentValues
 import android.content.Context
 import android.content.Intent
 import android.content.pm.PackageManager
+import android.graphics.Bitmap
 import android.graphics.Canvas
+import android.net.Uri
 import android.os.Bundle
+import android.provider.MediaStore
 import android.view.*
 import android.widget.Toast
 import androidx.core.app.ActivityCompat
@@ -16,6 +19,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.view.isVisible
 import androidx.fragment.app.activityViewModels
 import com.example.openlog.R
 import com.jjoe64.graphview.GridLabelRenderer
@@ -96,20 +100,28 @@ class DisplayGraph : Fragment() {
         val msInDay = 86400000
         val buffer = msInDay/100
 
-        when {
-            startDate.year != endDate.year -> {
-                pattern = "yy:MM"
-                if (endDate.time - startDate.time < (msInDay * 24 + buffer)) {
-                    pattern = addToPattern(pattern, "dd")
+        if (endDate != null) {
+            if (startDate != null) {
+                when {
+                    startDate.year != endDate.year -> {
+                        pattern = "yy:MM"
+                        if (endDate.time - startDate.time < (msInDay * 24 + buffer)) {
+                            pattern = addToPattern(pattern, "dd")
+                        }
+                    }
+                    startDate.month != endDate.month -> pattern = "MM:dd"
+                    startDate.date != endDate.date -> pattern = "dd:HH"
+
                 }
             }
-            startDate.month != endDate.month -> pattern = "MM:dd"
-            startDate.date != endDate.date -> pattern = "dd:HH"
-
         }
         if (pattern != "dd:HH") {
-            if (endDate.time - startDate.time < (msInDay * 3 + buffer))
-                pattern = addToPattern(pattern, "HH")
+            if (endDate != null) {
+                if (startDate != null) {
+                    if (endDate.time - startDate.time < (msInDay * 3 + buffer))
+                        pattern = addToPattern(pattern, "HH")
+                }
+            }
         }
         return pattern
     }
